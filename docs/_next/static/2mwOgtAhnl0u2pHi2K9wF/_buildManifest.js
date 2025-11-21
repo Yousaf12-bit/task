2b6acf42f97f66d43ac0598af5c33b7f46f5a15e
Yousaf Hassan <yousaf.hassan2015@gmail.com>
2025-11-21 11:51:52 +0500

    init

---
+++ docs/_next/static/2mwOgtAhnl0u2pHi2K9wF/_buildManifest.js
@@ -0,0 +1,24 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/382aa7f998d88ccf.js"
+  ],
+  "/_error": [
+    "static/chunks/b227b78bc8648cf5.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/task//_next/:path+",
+        "destination": "/task/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/hello"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ docs/_next/static/chunks/538dbe3e2dfa70c7.js
@@ -0,0 +1,6 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/b82da1b8ebcf39bd.js",
+  "static/chunks/2ce171fc40fd88e4.js",
+  "static/chunks/176614537f1ec682.css",
+  "static/chunks/turbopack-ecb840f529c46984.js"
+])
